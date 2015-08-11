9b9e1ed2e16de1b5ccecb8df59fb6c84a4316ac2
Aileen Cacayorin & Jason Lane <student@epicodus.com>
2015-08-11 16:40:46 -0700

    Use meta refresh to call time_passes

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require './lib/tamagotchi'
 also_reload('lib/**/*.rb')
 
 get('/') do
+  Tamagotchi.clear
  erb(:index)
 end
 get '/feed' do
@@ -12,9 +13,27 @@ get '/feed' do
   erb :tamagotchi
 end
 
+get '/play' do
+  @pet = Tamagotchi.retrieve_pet
+  @pet.play
+  erb :tamagotchi
+end
+
 post '/tamagotchi' do
 
   @pet = Tamagotchi.new(params['name'])
   @pet.save
+  erb :tamagotchi
+end
+
+get '/sleep' do
+  @pet = Tamagotchi.retrieve_pet
+  @pet.asleep
+  erb :tamagotchi
+end
+
+get '/tamagotchi' do
+  @pet = Tamagotchi.retrieve_pet
+  @pet.time_passes
   erb :tamagotchi
 end
